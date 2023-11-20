e7ac814ac1ecea935cc301818cb648b64e360866
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-19 19:28:31 -0800

    Fixed image bug

--- app/page.tsx
+++ app/page.tsx
@@ -35,6 +35,7 @@ export default function Page() {
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
+            src="/hero-mobile.png" 
             width={560}
             height={620}
             className="block md:hidden"

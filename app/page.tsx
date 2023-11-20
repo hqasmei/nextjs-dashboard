445c915e319b8edbef074a0d541b21d335aadabf
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-19 19:25:40 -0800

    Fixed alt bug

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,6 @@ export default function Page() {
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/hero-mobile.png"
             width={560}
             height={620}
             className="block md:hidden"
